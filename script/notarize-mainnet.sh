87a8d1e1c3e098eaa4770b1e66529c6c25a31105
fr1t2 <fr1t2@protonmail.com>
2023-03-03 15:43:04 -0700

    Use  notation instead of legacy backticked .

--- script/notarize-mainnet.sh
+++ script/notarize-mainnet.sh
@@ -26,19 +26,19 @@ STATS_FILE=$BACKUP_PATH/$NET_NAME/QRL_Node_Stats.json
 CHECKSUM_FILE=$BACKUP_PATH/$NET_NAME/"$NET_NAME"_State_Checksums.txt
 BOOTSTRAP_LOGS=$BACKUP_PATH/qrl_bootstrap.logs
 
-echo "["`date -u`"] QRL.CO.IN Notarize Script" |tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] QRL.CO.IN Notarize Script" |tee -a $BOOTSTRAP_LOGS
 echo "----------------------------------------------" | tee -a $BOOTSTRAP_LOGS 
 echo "Notarize checksums for $NET_NAME bootstrap to Cloud" | tee -a $BOOTSTRAP_LOGS  
 echo "----------------------------------------------" | tee -a $BOOTSTRAP_LOGS  
 
 # Check for address/wallet and if not found generate
 if [[ ! -d $QRL_DIR ]]; then
-  echo -e "["`date -u`"] QRL Directory Not Found! Should be at: $QRL_DIR/$QRL_WALLET" | tee -a $BOOTSTRAP_LOGS
+  echo -e "["$(date -u)"] QRL Directory Not Found! Should be at: $QRL_DIR/$QRL_WALLET" | tee -a $BOOTSTRAP_LOGS
   exit 1
 fi
 
 if [[ ! -f $QRL_DIR/$QRL_WALLET ]]; then
-  echo "["`date -u`"] Wallet not found! Generating New Address at $QRL_DIR/$QRL_WALLET" | tee -a $BOOTSTRAP_LOGS
+  echo "["$(date -u)"] Wallet not found! Generating New Address at $QRL_DIR/$QRL_WALLET" | tee -a $BOOTSTRAP_LOGS
   sudo -H -u $user qrl-cli create-wallet -f $QRL_DIR/$QRL_WALLET -h 14 # generate qrl address using the qrl-cli, tree height 14
   wallet_success=$?
   echo "["`date`"] create-wallet exit code: $copy_success" | tee -a $BOOTSTRAP_LOGS
@@ -49,35 +49,35 @@ if [[ ! -f $QRL_DIR/$QRL_WALLET ]]; then
 fi
 
 QRL_ADDRESS=$(cat $QRL_DIR/$QRL_WALLET |jq .[0].address | tr -d '"')
-echo "["`date -u`"] QRL Address: $QRL_ADDRESS" |tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] QRL Address: $QRL_ADDRESS" |tee -a $BOOTSTRAP_LOGS
 
 # Get next OTS
 OTS_KEY=$(sudo -H -u $user qrl-cli ots $QRL_ADDRESS -m -j |grep next_key |jq .[0].next_key)
-echo "["`date -u`"] Next unused OTS key: $OTS_KEY" |tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] Next unused OTS key: $OTS_KEY" |tee -a $BOOTSTRAP_LOGS
 
 # Get shasum of file
 SHASUM=$(sha256sum $CHECKSUM_FILE | awk '{print $1}')
-echo "["`date -u`"] sha256sum: $SHASUM" |tee -a $BOOTSTRAP_LOGS
-echo "["`date -u`"] Notarizing file on-chain" |tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] sha256sum: $SHASUM" |tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] Notarizing file on-chain" |tee -a $BOOTSTRAP_LOGS
 
 # Notarize shasum of checksum file
 NOTARIZE=$(sudo -H -u $user qrl-cli notarize $SHASUM -m -M "https://qrl.co.in/chain/ Mainnet Checksums" -w $QRL_DIR/$QRL_WALLET -i $OTS_KEY -j )
-echo "["`date -u`"] Notarization complete:" |tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] Notarization complete:" |tee -a $BOOTSTRAP_LOGS
 # Generate stats file
 TXID=$(echo $NOTARIZE |jq .[0].tx_id | tr -d '"')
-echo "["`date -u`"] QRL Transaction ID: $TXID" |tee -a $BOOTSTRAP_LOGS
-echo "["`date -u`"] Transaction Verification: https://explorer.theqrl.org/tx/$TXID" |tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] QRL Transaction ID: $TXID" |tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] Transaction Verification: https://explorer.theqrl.org/tx/$TXID" |tee -a $BOOTSTRAP_LOGS
 
 # Grab the chain state
 CHAIN_STATE=$(sudo -H -u $user /home/$user/.local/bin/qrl --json state)
 
 # remove the old stats file
 if [ -f $STATS_FILE ]; then
-  echo "["`date -u`"] Removing old stats file from: $STATS_FILE" | tee -a $BOOTSTRAP_LOGS  
+  echo "["$(date -u)"] Removing old stats file from: $STATS_FILE" | tee -a $BOOTSTRAP_LOGS  
   rm -rf $STATS_FILE
 fi
 
-echo "["`date -u`"] Writing NEW stats file to: $STATS_FILE" | tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] Writing NEW stats file to: $STATS_FILE" | tee -a $BOOTSTRAP_LOGS
 cat << EoF > $STATS_FILE
 [
     {"info":
@@ -99,7 +99,7 @@ cat << EoF > $STATS_FILE
 ]
 EoF
 
-echo "["`date -u`"] QRL $NET_NAME Chain StateFile Created" |tee -a $BOOTSTRAP_LOGS
+echo "["$(date -u)"] QRL $NET_NAME Chain StateFile Created" |tee -a $BOOTSTRAP_LOGS
 
 
 
