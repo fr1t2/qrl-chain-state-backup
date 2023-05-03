547c4915e7d1af679ca9efc2c6622e8c94c8b0ff
fr1t2 <fr1t2@protonmail.com>
2023-05-03 13:18:10 -0600

    Update backup-testnet.sh

--- backup-testnet.sh
+++ backup-testnet.sh
@@ -3,7 +3,7 @@
 # Backup, notarize and upload the testnet chain
 # Uses the various scripts to perform the backup located in teh /scripts dir
 
-user=fr1t2
+user="fr1t2"
 BACKUP_PATH=/home/"$user"/qrl-chain-state-backup/qrl_bootstrap_files
 BOOTSTRAP_LOGS="$BACKUP_PATH"/qrl_bootstrap.logs
 
@@ -15,6 +15,7 @@ echo "----------------------------------------------" | tee -a "$BOOTSTRAP_LOGS"
 
 ## Backup the chain state
 echo "Backup Chain....." | tee -a "$BOOTSTRAP_LOGS"  
+sudo -H -u "$user" pwd
 sudo -H -u "$user" /home/"$user"/qrl-chain-state-backup/CreateQRLBootstrap-testnet.sh
 echo "Backup Chain Complete!" | tee -a "$BOOTSTRAP_LOGS"  
 
@@ -26,4 +27,4 @@ echo "Notarize Complete!" | tee -a "$BOOTSTRAP_LOGS"
 ## Load to the Cloud
 echo "Upload to Cloud....." | tee -a "$BOOTSTRAP_LOGS"  
 sudo -H -u "$user" /home/"$user"/qrl-chain-state-backup/script/cloudUp-testnet.sh
-echo "Upload to Cloud Complete!" | tee -a "$BOOTSTRAP_LOGS"  
+echo "Upload to Cloud Complete!" | tee -a "$BOOTSTRAP_LOGS"
